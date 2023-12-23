5cc9d92d50ebf3e98dae6442355e1f02ad7d6a07
GitHubAvailable <lhzmark@163.com>
2023-12-23 18:44:48 -0500

    Initial Commit

---
+++ WordsHelper/WordsHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsHelperApp.swift
+//  WordsHelper
+//
+//  Created by Mark Liu on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct WordsHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
